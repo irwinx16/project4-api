7931f45c771fd153521deddc3973aee1f7c63392
Irwin <irgranados16@gmail.com>
2018-05-29 15:37:48 -0500

    login-register page working

--- src/App.js
+++ src/App.js
@@ -3,14 +3,63 @@ import './App.css';
 
 // Components
 import ProductContainer from './ProductContainer'
-// import LoginRegister from './LoginRegister';
+import LoginRegister from './LoginRegister';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      loggedIn: false,
+      logInErrorMessage: ''
     }
+  }
+
+  doLogin = async (username, password) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Response
+    const loginJson = await fetch('http://localhost:9292/user/login', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+
+    const loggedIn = await loginJson.json();
+      // console.log(loggedIn);
+      if (loggedIn.success) {
+        this.setState({
+          loggedIn: true,
+          logInErrorMessage: '',
+          message: `Welcome back, ${username}!`
+        })
+      } else {
+          this.setState({
+            logInErrorMessage: loggedIn.message
+          });
+        }
+  }
+
+  doRegister = async (username, password) => {
+    const registerJson = await fetch('http://localhost:9292/user/register', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    const parsedRegisterResponse = await registerJson.json();
+      if (parsedRegisterResponse.success) {
+        this.setState({
+          loggedIn: true,
+          message: `Welcome to the site, ${username}!`
+        })
+      } else {
+          this.setState({
+            logInErrorMessage: parsedRegisterResponse.message
+          })
+        }
   }
 
 
@@ -20,7 +69,13 @@ class App extends Component {
     console.log(this.state, "this is the app.js container");
     return (
       <div className="App">
+        {this.state.loggedIn ?
         <ProductContainer/>
+
+        : <LoginRegister doLogin={this.doLogin} doRegister={this.doRegister} />
+
+        }
+
 
 
       </div>
--- src/LoginRegister/index.js
+++ src/LoginRegister/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class LoginRegister extends Component {
   constructor() {
@@ -6,8 +7,25 @@ class LoginRegister extends Component {
     this.state = {
       username: '',
       password: '',
-      registering: false,
+      register: false
     }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.register) this.props.doRegister(this.state.username, this.state.password)
+    else this.props.doLogin(this.state.username, this.state.password)
+  }
+
+  handleInput = (e) => {
+    const field = e.currentTarget.name
+    if (field === "username") this.setState({"username": e.currentTarget.value})
+      else this.setState({"password": e.currentTarget.value})
+  }
+  registration = (e) => {
+    this.setState({register: true});
+  }
+  loggingIn = (e) => {
+    this.setState({register: false});
   }
 
 
@@ -15,8 +33,14 @@ class LoginRegister extends Component {
   render() {
     return (
       <div>
-        <h1> Project 4 Title</h1>
+        <h1> This is login/-register.</h1>
+        <p><span className={this.state.register ? "current" : null} onClick={this.registration}>Register</span> • <span className={!this.state.register ? "current" : null} onClick={this.loggingIn}>Log In</span></p>
 
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleInput} /> <br/>
+          <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInput} /> <br/>
+          <button>{this.state.register ? "Register" : "Login"}</button>
+        </form>
 
       </div>
     )
